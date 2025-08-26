a0fedaebf0b11631f690dd4b4e799910c33d2bfb
abhishektumula <abhishektumula@gmail.com>
2025-08-26 22:24:23 +0530

    feat: may be good bye to Ubuntu...

--- init.lua
+++ init.lua
@@ -8,7 +8,7 @@ vim.opt.cmdheight = 1
 vim.opt.colorcolumn = "80"
 vim.opt.showmode = true
 -- important
-vim.cmd.colorscheme("everforest")
+vim.cmd.colorscheme("rose-pine")
 -- important
 vim.opt.scrolloff = 7
 -- Make all keywords bold
@@ -20,6 +20,27 @@ vim.cmd("highlight Type gui=bold cterm=bold")
 vim.cmd("highlight Constant gui=bold cterm=bold")
 vim.cmd("highlight Identifier gui=bold cterm=bold")
 vim.opt.guicursor = "a:block"
+
+-- Auto-remove extra blank lines at EOF, keep max 9
+
+-- Ensure exactly 9 blank lines at EOF
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    local save_cursor = vim.fn.getpos(".")
+    vim.cmd([[
+      keepjumps keeppatterns %s/\(\n\)\{1,}\%$//e
+      for i in range(9)
+        $put =''
+      endfor
+    ]])
+    vim.fn.setpos(".", save_cursor)
+  end,
+})
+
+
+
+
 --Universal transparency overrides
 local transparent_groups = {
   "Normal",
